46a0827130a2763c5c468da7226db6ccf14d29cb
Alexsandro <alexsandrodesenvolvedor@gmail.com>
2020-10-16 19:37:37 -0300

    Refactory

--- auth/src/main/java/com/arquitetura/auth/config/AuthenticationFailureListener.java
+++ auth/src/main/java/com/arquitetura/auth/config/AuthenticationFailureListener.java
@@ -2,10 +2,8 @@ package com.arquitetura.auth.config;
 
 import com.arquitetura.auth.services.LoginAttemptService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.security.authentication.event.AuthenticationFailureBadCredentialsEvent;
-import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
